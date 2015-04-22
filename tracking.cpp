89941f96f8abde1b9e4f9aeb832f4c025e6f30ab
ximenes <xresende@gmail.com>
2015-04-21 23:27:54 -0300

    added findorbit4
    - not completed.

--- tracking.cpp
+++ tracking.cpp
@@ -182,6 +182,88 @@ Status::type track_findorbit6(
 
 }
 
+Status::type track_findorbit4(
+    const Accelerator& accelerator,
+    const double& de,
+    std::vector<Pos<double> >& closed_orbit,
+    const Pos<double>& fixed_point_guess) {
+
+  const std::vector<Element>& the_ring = accelerator.lattice;
+
+  double delta        = 1e-9;              // [m],[rad],[dE/E]
+  double tolerance    = 2.22044604925e-14;
+  int    max_nr_iters = 50;
+
+  // temporary vectors and matrices
+  // std::vector<Pos<double> > co(7,0); // no initial guess
+  std::vector<Pos<double> > co(7,fixed_point_guess);
+  std::vector<Pos<double> > co2(7,0);
+  std::vector<Pos<double> > D(7,0);
+  std::vector<Pos<double> > M(6,0);
+  Pos<double> dco(1.0,1.0,1.0,1.0,0.0,0.0);
+  Pos<double> theta(0.0,0.0,0.0,0.0,0.0,0.0);
+  theta.dl = fixedpoint;
+  matrix6_set_identity(D, delta);
+
+  int nr_iter = 0;
+  while ((get_max(dco) > tolerance) and (nr_iter <= max_nr_iters)) {
+    co = co + D;
+    Pos<double> Ri = co[6];
+    std::vector<Pos<double> > co2;
+    unsigned int element_offset = 0;
+    Plane::type lost_plane;
+    Status::type status = Status::success;
+    status = (Status::type) ((int) status | (int) track_linepass(accelerator, co[0], co2, element_offset, lost_plane, false));
+    status = (Status::type) ((int) status | (int) track_linepass(accelerator, co[1], co2, element_offset, lost_plane, false));
+    status = (Status::type) ((int) status | (int) track_linepass(accelerator, co[2], co2, element_offset, lost_plane, false));
+    status = (Status::type) ((int) status | (int) track_linepass(accelerator, co[3], co2, element_offset, lost_plane, false));
+    status = (Status::type) ((int) status | (int) track_linepass(accelerator, co[4], co2, element_offset, lost_plane, false));
+    status = (Status::type) ((int) status | (int) track_linepass(accelerator, co[5], co2, element_offset, lost_plane, false));
+    //printf("%.4e %.4e %.4e %.4e %.4e %.4e\n", co[6].rx, co[6].px, co[6].ry, co[6].py, co[6].de, co[6].dl);
+    status = (Status::type) ((int) status | (int) track_linepass(accelerator, co[6], co2, element_offset, lost_plane, false));
+    if (status != Status::success) {
+      //printf("nr_iter: %i\n", nr_iter);
+      //printf("element: %i\n", element_offset);
+      //printf("plane: %i\n", lost_plane);
+      return Status::findorbit_one_turn_matrix_problem;
+    }
+    //print(co2);
+    Pos<double> Rf = co2[6];
+    M[0] = (co2[0] - Rf) / delta;
+    M[1] = (co2[1] - Rf) / delta;
+    M[2] = (co2[2] - Rf) / delta;
+    M[3] = (co2[3] - Rf) / delta;
+    M[4] = (co2[4] - Rf) / delta;
+    M[5] = (co2[5] - Rf) / delta;
+    //print(M);
+    Pos<double> b = Rf - Ri - theta;
+    std::vector<Pos<double> > M_1(6,0);
+    matrix6_set_identity(M_1);
+    M_1 = M_1 - M;
+    dco = linalg_solve(M_1, b);
+    //printf("%.4e %.4e %.4e %.4e %.4e %.4e\n", Rf.rx, Rf.px, Rf.ry, Rf.py, Rf.de, Rf.dl);
+    //printf("%.4e %.4e %.4e %.4e %.4e %.4e\n", b.rx, b.px, b.ry, b.py, b.de, b.dl);
+    //printf("%.4e %.4e %.4e %.4e %.4e %.4e\n", dco.rx, dco.px, dco.ry, dco.py, dco.de, dco.dl);
+    co[6] = dco + Ri;
+    co[0] = co[6]; co[1] = co[6];
+    co[2] = co[6]; co[3] = co[6];
+    co[4] = co[6]; co[5] = co[6];
+    nr_iter++;
+  }
+
+  if (nr_iter > max_nr_iters) {
+    return Status::findorbit_not_converged;
+  }
+
+  // propagates fixed point throught the_ring
+  closed_orbit.clear();
+  unsigned int element_offset = 0;
+  Plane::type lost_plane;
+  track_linepass(accelerator, co[6], closed_orbit, element_offset, lost_plane, true);
+  closed_orbit.pop_back(); // eliminates last element which is the same as first
+  return Status::success;
+
+}
 
 Pos<double> linalg_solve(const std::vector<Pos<double> >& M, const Pos<double>& B) {
 
