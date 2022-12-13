34b2b124a6197e8dde0d67af4a46ab2af4592677
ximenes <xresende@gmail.com>
2022-12-13 18:10:07 -0300

    Fix line ordering

--- include/trackcpp/passmethods.hpp
+++ include/trackcpp/passmethods.hpp
@@ -260,8 +260,8 @@ template <typename T>
 Status::type pm_identity_pass(Pos<T> &pos, const Element &elem,
                               const Accelerator& accelerator) {
   global_2_local(pos, elem);
-  return Status::success;
   local_2_global(pos, elem);
+  return Status::success;
 }
 
 template <typename T>
