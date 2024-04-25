29b7dd52736eab8a18fba5f04f016f8cd2493bc0
Vitor <vitor.souza@lnls.br>
2024-04-25 14:03:58 -0300

    save

--- include/trackcpp/passmethods.hpp
+++ include/trackcpp/passmethods.hpp
@@ -245,20 +245,20 @@ inline void rotate_pos(Pos<T> &pos, const double* R) {
 
 template <typename T>
 void global_2_local(Pos<T> &pos, const Element &elem) {
-    if (elem.has_t_in) {
+    if (!elem.has_t_in) {} else {
         translate_pos(pos, elem.t_in);
     }
-    if (elem.has_r_in) {
+    if (!elem.has_r_in) {} else {
         rotate_pos(pos, elem.r_in);
     }
 }
 
 template <typename T>
 void local_2_global(Pos<T> &pos, const Element &elem) {
-    if (elem.has_r_out) {
+    if (!elem.has_r_out) {} else {
         rotate_pos(pos, elem.r_out);
     }
-    if (elem.has_t_out) {
+    if (!elem.has_t_out) {} else {
         translate_pos(pos, elem.t_out);
     }
 }
