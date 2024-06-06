b6dd5754b16341820e0767b6846a17bccd5be499
vitor <vitor.souza@lnls.br>
2024-06-06 12:26:16 -0300

    adjust in cav pass 1 comp

--- include/trackcpp/passmethods.hpp
+++ include/trackcpp/passmethods.hpp
@@ -426,6 +426,66 @@ Status::type pm_cavity_0_pass(Pos<T> &pos, const Element &elem,
   return Status::success;
 }
 
+// template <typename T>
+// Status::type pm_cavity_1comp_pass(Pos<T> &pos, const Element &elem,
+//                             const Accelerator& accelerator) {
+
+//   if (not accelerator.cavity_on) return pm_drift_pass(pos, elem, accelerator);
+
+//   global_2_local(pos, elem);
+//   double nv = elem.voltage / accelerator.energy;
+//   double frf = elem.frequency;
+//   double harmonic_number = accelerator.harmonic_number;
+//   double L0 = accelerator.get_length();
+//   double s = 0.0;
+//   double accum_s = 0.0;
+//   unsigned int last_cav_idx = 0;
+//   for (unsigned int i = 0; i < accelerator.lattice.size(); i++){
+//     auto elem2 = accelerator.lattice[i];
+//     if (elem2 == elem){
+//       accum_s += s;
+//     }
+//     if (elem2.frequency != 0.0){
+//       s = 0.0;
+//       last_cav_idx = i;
+//     }
+//     s += elem2.length;
+//   }
+//   double ddl = (light_speed*harmonic_number/frf - L0);
+//   if (elem.length == 0) {
+//     T &de = pos.de, &dl = pos.dl;
+//     dl -= ddl*(accum_s/L0);
+//     de +=  -nv * sin(TWOPI*frf *dl/ light_speed - elem.phase_lag);
+//     if (elem == accelerator.lattice[last_cav_idx]){
+//       dl -= ddl*(s/L0);
+//     }
+//     } else {
+//     T &rx = pos.rx, &px = pos.px;
+//     T &ry = pos.ry, &py = pos.py;
+//     T &de = pos.de, &dl = pos.dl;
+//     // drift half length
+//     T pnorm   = 1 / (1 + de);
+//     T norml   = (0.5 * elem.length) * pnorm;
+//     rx += norml * px;
+//     ry += norml * py;
+//     dl += 0.5 * norml * pnorm * (px*px + py*py);
+//     // longitudinal momentum kick
+//     dl -= ddl*(accum_s/L0);
+//     de += -nv * sin(TWOPI*frf *dl/ light_speed - elem.phase_lag);
+//     if (elem == accelerator.lattice[last_cav_idx]){
+//       dl -= ddl*(s/L0);
+//     }
+//     // drift half length
+//     pnorm   = 1.0 / (1.0 + de);
+//     norml   = (0.5 * elem.length) * pnorm;
+//     rx += norml * px;
+//     ry += norml * py;
+//     dl += 0.5 * norml * pnorm * (px*px + py*py);
+//   }
+//   local_2_global(pos, elem);
+//   return Status::success;
+// }
+
 template <typename T>
 Status::type pm_cavity_1comp_pass(Pos<T> &pos, const Element &elem,
                             const Accelerator& accelerator) {
@@ -437,11 +497,21 @@ Status::type pm_cavity_1comp_pass(Pos<T> &pos, const Element &elem,
   double frf = elem.frequency;
   double harmonic_number = accelerator.harmonic_number;
   double L0 = accelerator.get_length();
+  unsigned int last_cav_idx = 0;
+  for (unsigned int i = 0; i < accelerator.lattice.size(); i++){
+    auto elem2 = accelerator.lattice[i];
+    if (elem2.frequency != 0.0){
+      last_cav_idx = i;
+    }
+  }
   double ddl = (light_speed*harmonic_number/frf - L0);
   if (elem.length == 0) {
     T &de = pos.de, &dl = pos.dl;
+
     de +=  -nv * sin(TWOPI*frf *dl/ light_speed - elem.phase_lag);
-    dl -= ddl;
+    if (elem == accelerator.lattice[last_cav_idx]){
+      dl -= ddl;
+    }
     } else {
     T &rx = pos.rx, &px = pos.px;
     T &ry = pos.ry, &py = pos.py;
@@ -453,8 +523,11 @@ Status::type pm_cavity_1comp_pass(Pos<T> &pos, const Element &elem,
     ry += norml * py;
     dl += 0.5 * norml * pnorm * (px*px + py*py);
     // longitudinal momentum kick
+
     de += -nv * sin(TWOPI*frf *dl/ light_speed - elem.phase_lag);
-    dl -= ddl;
+    if (elem == accelerator.lattice[last_cav_idx]){
+      dl -= ddl;
+    }
     // drift half length
     pnorm   = 1.0 / (1.0 + de);
     norml   = (0.5 * elem.length) * pnorm;
@@ -481,8 +554,8 @@ Status::type pm_cavity_1frac_pass(Pos<T> &pos, const Element &elem,
   double ddl = (light_speed*harmonic_number/frf - L0)*time_aware_frac;
   if (elem.length == 0) {
     T &de = pos.de, &dl = pos.dl;
-    de +=  -nv * sin(TWOPI*frf *dl/ light_speed - elem.phase_lag);
     dl -= ddl;
+    de +=  -nv * sin(TWOPI*frf *dl/ light_speed - elem.phase_lag);
     } else {
     T &rx = pos.rx, &px = pos.px;
     T &ry = pos.ry, &py = pos.py;
@@ -494,8 +567,8 @@ Status::type pm_cavity_1frac_pass(Pos<T> &pos, const Element &elem,
     ry += norml * py;
     dl += 0.5 * norml * pnorm * (px*px + py*py);
     // longitudinal momentum kick
-    de += -nv * sin(TWOPI*frf *dl/ light_speed - elem.phase_lag);
     dl -= ddl;
+    de += -nv * sin(TWOPI*frf *dl/ light_speed - elem.phase_lag);
     // drift half length
     pnorm   = 1.0 / (1.0 + de);
     norml   = (0.5 * elem.length) * pnorm;
@@ -518,8 +591,8 @@ Status::type pm_cavity_2_pass(Pos<T> &pos, const Element &elem,
   double frf = elem.frequency;
   double harmonic_number = accelerator.harmonic_number;
   double L0 = accelerator.get_length();
-  double time_aware_frac = accelerator.get_time_aware_frac();
-  double ddl =  (light_speed*harmonic_number/frf - L0) * time_aware_frac * nturn;
+  // double time_aware_frac = accelerator.get_time_aware_frac();
+  double ddl =  (light_speed*harmonic_number/frf - L0)* nturn;// * time_aware_frac ;
   if (elem.length == 0) {
     T &de = pos.de, &dl = pos.dl;
     de +=  -nv * sin(TWOPI*frf *(dl-ddl)/ light_speed - elem.phase_lag);
