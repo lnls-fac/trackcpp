0e574f441c8f8a5f73f9bc3dcea201d85375d944
Vitor <vitor.souza@lnls.br>
2025-09-12 14:45:39 -0300

    MNT: beautify "is_time_aware_pm" function

--- include/trackcpp/auxiliary.h
+++ include/trackcpp/auxiliary.h
@@ -23,6 +23,7 @@
 #include <iostream>
 #include <cmath>
 #include <random>
+#include <algorithm>
 
 class PassMethodsClass {
 public:
@@ -55,10 +56,7 @@ public:
     int size() const { return passmethods.size(); }
     std::string operator[](const int i) const { return passmethods[i]; }
     bool is_time_aware_pm(const int i) const {
-        for (int pm: time_aware_passmethods) {
-            if (i == pm) {return true;}
-        }
-        return false;
+        return std::find(time_aware_passmethods.begin(), time_aware_passmethods.end(), i) != time_aware_passmethods.end();
     };
 private:
     std::vector<std::string> passmethods;
