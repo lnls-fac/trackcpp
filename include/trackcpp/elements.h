d8cdf1d5bd606bfcb739dafb45378093e707d265
Gabriel <gabriel.ascencao@lnls.br>
2025-10-30 15:41:38 -0300

    change coefs in elements

--- include/trackcpp/elements.h
+++ include/trackcpp/elements.h
@@ -65,8 +65,8 @@ public:
   double        rescale_kicks = 1.0;  // for kickmaps
   double        ks = 0;       // [1/m]
   double        kx = 0;       // [1/m]
-  double        s0 = 0;   // [m]
-  std::vector<std::vector<double>> coefs = std::vector<std::vector<double>>(5, std::vector<double>(5, 0.0));
+  double        s0 = 0;       // [m]
+  std::vector<std::vector<double>> coefs;
   
   std::vector<double> polynom_a = default_polynom;
   std::vector<double> polynom_b = default_polynom;
